1cb58458b1c859166e782b1be6ab6a0a83d5894b
kvgabi4 <kvgabi4@gmail.com>
2020-11-16 20:44:59 +0100

    correction

--- js/5-count-evens.js
+++ js/5-count-evens.js
@@ -10,7 +10,7 @@ const intArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function sumElementsOfAnArray(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i += 1) {
-        if (arr[i] % 2 == 0) {
+        if (arr[i] % 2 === 0) {
             sum += 1;
         }
     }
